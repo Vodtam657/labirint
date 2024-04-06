2eaa215605b86cd50f3b065f2fc82ff91948b78c
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2024-04-06 18:19:38 +0300

    \

--- venv.py
+++ venv.py
@@ -1,8 +1,25 @@
-ipmort pygame
+import pygame
 
 window = pygame.display.set_mode((700, 500))
 fps = pygame.time.Clock()
 
 
-window.dilplay.flip
 
+background = pygame.transform.scale(pygame.image.load("images (2).jpg"), (700, 500))
+
+
+
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    window.blit(background, (0, 0))
+    roma.draw(window)
+    roma.move()
+    salabai.draw(window)
+    salabai.move()
+
+    pygame.display.flip()
+    fps.tick(60)
